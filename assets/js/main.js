2bef9b072680435b856c2a2a168f8a48393a4624
Nedim Arabacı <nedimarabaci@gmail.com>
2014-01-04 00:10:06 +0200

    new look

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,38 +18,32 @@
 		};
 
 		this.showSection = function (id) {
-
 			var section = this.getSection(id);
-			var sectionHeight = section.height();
-			var windowHeight = $(window).height();
-			var actualTopMargin = (windowHeight / 2) - sectionHeight + 100;
 
-			section.css('padding-top', actualTopMargin + 'px');
-
-			$('.section').on('mouseenter', '.moving-letter', function(e) {
+			$('.section').on('mouseenter', '.moving-letter', function (e) {
 				$(this).trigger('reset').transition({
 					perspective: '100px',
-					rotateY: '+=360deg'
+					rotateY    : '+=360deg'
 				});
 			});
 
 			section.transition({
-				y: '100px',
+				y      : '-=50px',
 				opacity: 1
-			}, 1000, function() {
-				$.each(section.find('span'), function(i, _span) {
+			}, 1000, function () {
+				$.each(section.find('span'), function (i, _span) {
 					var span = $(_span);
 
-					span.children().andSelf().contents().each(function(){
+					span.children().andSelf().contents().each(function () {
 						if (this.nodeType == 3) {
 							var $this = $(this);
 							$this.replaceWith($this.text().replace(/(\w)/g, "<span class='moving-letter'>$&</span>"));
 						}
 					});
 
-					$.each(span.find('.moving-letter'), function(i, ml) {
+					$.each(span.find('.moving-letter'), function (i, ml) {
 						i++;
-						setTimeout(function() {
+						setTimeout(function () {
 							$(ml).addClass('colored').trigger('mouseenter');
 						}, i * ((Math.random() * 10) + 1) * 45);
 					});
@@ -66,5 +60,9 @@
 
 	var App = new Application();
 	App.init();
+
+	$('.section').curtain(0, function (section) {
+//		console.log($(section).attr('id'));
+	});
 
 }());
