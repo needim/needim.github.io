75f10839c7dd9bab000870498c84df8ea1dcf847
Nedim ARABACI <nedim.arabaci@limitforex.com>
2019-04-14 16:36:46 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,64 @@
-'use strict'
+'use strict';
 
-const gulp = require('gulp')
-const watch = require('gulp-watch')
-const rigger = require('gulp-rigger')
-const browserSync = require('browser-sync')
-const prettify = require('gulp-jsbeautifier')
-const download = require('gulp-download-stream')
-const rename = require('gulp-rename')
-const jsonTransform = require('gulp-json-transform')
-const Showdown = require('showdown')
+const { src, dest, watch } = require('gulp');
+const rigger = require('gulp-rigger');
+const browserSync = require('browser-sync');
+const download = require('gulp-download-stream');
+const rename = require('gulp-rename');
 
 const path = {
-  build: { // production
+  build: {
     html: './'
   },
-  src: { // development
+  src: {
     html: 'src/*.html'
   },
   watch: {
     html: 'src/**/*.html'
   }
-}
+};
 
-/* =====================================================
- SERVER
- ===================================================== */
-const config = {
-  server: {
-    baseDir: './'
-  },
-  tunnel: false,
-  host: 'localhost',
-  port: 9000,
-  logPrefix: 'NEDIM_PP',
-  open: false,
-  watchTask: true
-}
-
-gulp.task('webserver', () => {
-  browserSync(config)
-})
-
-/* =====================================================
- HTML
- ===================================================== */
-
-gulp.task('html:build', () => {
-  return gulp.src(path.src.html)
-    .pipe(rigger())
-    .pipe(prettify())
-    .pipe(gulp.dest(path.build.html))
-})
-
-/* =====================================================
- RELEASES
- ===================================================== */
-
-gulp.task('download:gitawards', () => {
-  return download('http://git-awards.com/api/v0/users/needim', {
+function downloadStats() {
+  download('http://git-awards.com/api/v0/users/needim', {
     headers: {
       'User-Agent': 'ned.im personal page'
     }
   })
     .pipe(rename('gitawards.json'))
-    .pipe(gulp.dest(path.build.html))
-})
+    .pipe(dest(path.build.html));
+}
 
+function html() {
+  return src(path.src.html)
+    .pipe(rigger())
+    .pipe(dest(path.build.html));
+}
 
-/* =====================================================
- BUILD TASK
- ===================================================== */
+function serve() {
+  const config = {
+    server: {
+      baseDir: './'
+    },
+    tunnel: false,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: 'needim-pp',
+    open: false,
+    watch: true
+  };
 
-gulp.task('build', ['html:build'])
+  downloadStats();
+  html();
+  watchHTML();
+  browserSync.init(config);
+}
 
-/* =====================================================
- WATCH
- ===================================================== */
+function watchHTML() {
+  return watch(path.watch.html).on('change', () => {
+    html();
+  });
+}
 
-gulp.task('watch', () => {
-  watch([path.watch.html], (event, cb) => {
-    gulp.start('html:build')
-  })
-})
-
-/* =====================================================
- DEFAULT TASK
- ===================================================== */
-
-gulp.task('default', ['download:gitawards', 'build', 'webserver', 'watch'])
+exports.download = downloadStats;
+exports.html = html;
+exports.default = serve;
